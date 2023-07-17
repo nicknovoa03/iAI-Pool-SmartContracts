86b9785754171af77d66f2d062e14a3f689697a1
Nick Novoa <nicknovoa03@gmail.com>
2023-07-17 15:51:01 -0500

    initial Commit

---
+++ scripts/deployiAIPool.ts
@@ -0,0 +1,21 @@
+import { ethers } from "hardhat";
+
+async function main() {
+
+  const iAIPool = await ethers.getContractFactory("iAIPool");
+  const contract = await iAIPool.deploy("");
+
+  await contract.deployed();
+
+  console.log(`Contract deployed to ${contract.address}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
+
+
