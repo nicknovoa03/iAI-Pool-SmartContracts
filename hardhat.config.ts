9a7456bc03c1898624623b9fbdb774eea2970651
Nick Novoa <nicknovoa03@gmail.com>
2023-07-27 12:09:10 -0500

    remove unused vars

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,14 +3,12 @@ import "@nomicfoundation/hardhat-toolbox";
 import dotenv from 'dotenv';
 dotenv.config();
 
-const BscPrivateKey = process.env.BSC_PRIVATE_KEY!
 const EthPrivateKey = process.env.ETH_PRIVATE_KEY!
 const EthTestPrivateKey = process.env.ETH_TEST_PRIVATE_KEY!
 const EtherscanApiKey = process.env.ETHERSCAN_API_KEY!
 
 const BscProvider = "https://bsc-dataseed.binance.org/"
 const EthProvider = "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY
-const BscTestnetProvider = "https://data-seed-prebsc-1-s1.binance.org:8545"
 const EthTestnetProvider = "https://sepolia.infura.io/v3/" + process.env.INFURA_API_KEY
 
 const config: HardhatUserConfig = {
@@ -19,10 +17,6 @@ const config: HardhatUserConfig = {
     apiKey: EtherscanApiKey
   },
   networks: {
-    bsc: {
-      url: BscProvider,
-      accounts: [BscPrivateKey],
-    },
     sepolia: {
       url: EthTestnetProvider,
       accounts: [EthTestPrivateKey],
